5354e08fc959aa7e51deae6e4276ce29e50fba3f
Lorenzo Rutayisire <loryruta23@gmail.com>
2021-04-07 12:37:28 +0200

    Fix resource bin assoc

--- src/resources/resource_manager.hpp
+++ src/resources/resource_manager.hpp
@@ -19,8 +19,16 @@ namespace rge
 	{
 		namespace detail
 		{
+			class bin_assoc_hash_func {
+			public:
+				size_t operator()(std::pair<Resource, Resource> const& key) const
+				{
+					return ((uint64_t) key.first) << 32 | ((uint64_t) key.second);
+				}
+			};
+
 			inline std::unordered_map<Resource, ResourceDescriptor> s_resource_descriptors;
-			inline std::unordered_map<uint64_t, Resource> s_resource_binary_assoc;
+			inline std::unordered_map<std::pair<Resource, Resource>, Resource, bin_assoc_hash_func> s_resource_binary_assoc;
 		}
 
 		template<typename R>
@@ -52,15 +60,15 @@ namespace rge
 		template<is_resource_type A, is_resource_type B, is_resource_type R>
 		inline void register_resource_binary_assoc(A res_a, B res_b, R res_c)
 		{
-			detail::s_resource_binary_assoc.emplace(((uint64_t) res_a) << 32 | (uint64_t) res_b, (Resource) res_c);
+			auto key = std::pair((Resource) res_a, (Resource) res_b);
+			detail::s_resource_binary_assoc.emplace(key, (Resource) res_c);
 		}
 
 		template<is_resource_type A, is_resource_type B, is_resource_type R>
 		inline R get_resource_from_assoc(A res_a, B res_b)
 		{
-			Resource res_a_id = static_cast<Resource>(res_a);
-			Resource res_b_id = static_cast<Resource>(res_b);
-			return static_cast<R>(detail::s_resource_binary_assoc[((uint32_t) res_a_id) << 16 | res_b_id]);
+			auto key = std::pair((Resource) res_a, (Resource) res_b);
+			return static_cast<R>(detail::s_resource_binary_assoc[key]);
 		}
 
 		inline resources::MaterialShader get_material_shader(resources::Shader shader, resources::Material material)
@@ -70,7 +78,7 @@ namespace rge
 
 		inline resources::MaterialShader get_material_shader(resources::Shader shader, Material const* material)
 		{
-			return get_material_shader(shader, (resources::Material)material->get_resource());
+			return get_material_shader(shader, (resources::Material) material->get_resource());
 		}
 	};
 }
